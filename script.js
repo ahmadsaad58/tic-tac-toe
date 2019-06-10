47db02b404b1a81c36956ac3cd6b539ae3e9cfb7
Saad Ahmad <ahmadsaad58@gmail.com>
2019-06-10 17:42:28 -0500

    might add cookies

--- script.js
+++ script.js
@@ -166,3 +166,39 @@ function declareWinner(player) {
 	document.querySelector(".endgame").style.display = "block";
 	document.querySelector(".endgame .text").innerText = player;
 }
+
+// function checkCookie() {
+// 	var user = getCookie("username");
+// 	if (user != "") {
+// 		alert("Welcome again " + user);
+// 	} else {
+// 		user = prompt("Please enter your name:", "");
+// 		if (user != "" && user != null) {
+// 			setCookie("username", user, 30);
+// 		}
+// 	}
+// }
+
+// function setCookie(cname, cvalue, exdays) {
+// 	var d = new Date();
+// 	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+// 	var expires = "expires=" + d.toGMTString();
+// 	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// }
+
+// function getCookie(cname) {
+// 	var name = cname + "=";
+// 	console.log(document.cookie);
+// 	var decodedCookie = decodeURIComponent(document.cookie);
+// 	var ca = decodedCookie.split(';');
+// 	for (var i = 0; i < ca.length; i++) {
+// 		var c = ca[i];
+// 		while (c.charAt(0) == ' ') {
+// 			c = c.substring(1);
+// 		}
+// 		if (c.indexOf(name) == 0) {
+// 			return c.substring(name.length, c.length);
+// 		}
+// 	}
+// 	return "";
+// }
